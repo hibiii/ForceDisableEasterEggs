eecd3b55d4e7980fe31b8d92af3ae66ed7b47654
hibi <69405847+hibiii@users.noreply.github.com>
2023-12-24 13:36:11 -0300

    hello, world

---
+++ src/main/java/hibi/forcedisableeastereggs/Main.java
@@ -0,0 +1,13 @@
+package hibi.forcedisableeastereggs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    
+    public static Logger LOGGER = LoggerFactory.getLogger("ForceDisableEasterEggs");
+
+    public void initializeClient() {
+        LOGGER.info("hello, world");
+    }
+}
